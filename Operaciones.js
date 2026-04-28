cc9b13890587ac9ddf3bca0bf650d05ad12faa15
Ana Ruth Rivera <anaruth.riverasalgado@gmail.com>
2026-04-27 20:08:34 -0600

    funcionan las operaciones de suma, resta, multiplicacion, division creo

--- Operaciones.js
+++ Operaciones.js
@@ -1,72 +1,96 @@
 let RightNumber = "";
 let LeftNumber = "";
 let TempResult;
-let Operator;
-
 
 function Calcular(fullAr) {
     TempString = fullAr;
 
-    while (fullAr.includes("*")) {
-        TempString = calcularMult(TempString);
+    while (TempString.includes("*")) {
+        TempString = subCalcular(TempString, "*");
     }
 
+    while (TempString.includes("/")) {
+        TempString = subCalcular(TempString, "/");
+    }
 
+    while (TempString.includes("+")) {
+        TempString = subCalcular(TempString, "+");
+    }
+
+    while (TempString.includes("-")) {
+        if (TempString[0] === '-') { break; }
+        TempString = subCalcular(TempString, "-");
+    }
+
+    return TempString
 }
 
-function calcularMult(arr) {
-    RightNumber = " ";
-    LeftNumber = " ";
+function subCalcular(arr, symbol) {
+    RightNumber = "";
+    LeftNumber = "";
 
-    index = arr.indexOf("*");
+    index = arr.indexOf(symbol);
     inBegin = index;
     inEnd = index;
 
-    // a donde inicia la multiplicacion
-    ch = '0';
-    while (ch >= '0' && ch <= '9') {
-        inBegin--;
-        LeftNumber = arr[inBegin] + LeftNumber;
+    // a donde inicia 
+    while (inBegin > 0) {
         ch = arr[inBegin - 1];
+        if (ch >= '0' && ch <= '9') {
+            inBegin--;
+            LeftNumber = arr[inBegin] + LeftNumber;
+        } else if (ch === '-' && (inBegin - 1 === 0 || "+-*/".includes(arr[inBegin - 2]))) {
+            inBegin--;
+            LeftNumber = '-' + LeftNumber;
+            break;
+        }
+        else {
+            break;
+        }
     }
 
-    // a donde termina la multiplicacion
-    ch = '0';
-    while (ch >= '0' && ch <= '9') {
-        inEnd++;
-        RightNumber = RightNumber + arr[inEnd];
+    // a donde termina
+    while (inEnd < arr.length - 1) {
         ch = arr[inEnd + 1];
+        if (ch >= '0' && ch <= '9') {
+            inEnd++;
+            RightNumber = RightNumber + arr[inEnd];
+        } else if (ch === '-' && RightNumber === "" && (inEnd + 1 < arr.length - 1) && (arr[inEnd + 2] >= '0') && arr[inEnd + 2] <= '9') {
+            inEnd++;
+            RightNumber += '-';
+        }
+        else {
+            break;
+        }
     }
 
     BeginStr = arr.slice(0, inBegin);
     EndStr = arr.slice(inEnd + 1, arr.length);
 
-    TempResult = multiplicacion(parseInt(LeftNumber), parseInt(RightNumber));
+    switch (symbol) {
+        case "+":
+            TempResult = parseInt(LeftNumber) + parseInt(RightNumber);
+            break;
+        case "-":
+            if (LeftNumber === "") {
+                LeftNumber = "0";
+            }
+            TempResult = parseInt(LeftNumber) - parseInt(RightNumber);
+            break;
+        case "*":
+            TempResult = parseInt(LeftNumber) * parseInt(RightNumber);
+            break;
+        case "/":
+            if (parseInt(RightNumber) !== 0) {
+                TempResult = parseInt(LeftNumber) / parseInt(RightNumber);
+            } else {
+                return "Err";
+            }
+            break;
+    }
     return BeginStr + TempResult + EndStr;
 }
 
 
-
-
-
-function suma(a, b) {
-    return a + b;
-}
-
-function resta(a, b) {
-    return a - b;
-}
-
-function multiplicacion(a, b) {
-    return a * b;
-}
-
-function division(a, b) {
-    if (b !== 0) {
-        return a / b;
-    }
-    return "Error: Division por cero no permitida.";
-}
-
-let resultado = calcularMult("3+56*24-4");
+let resultado = Calcular("2-4*3+5/2");
 console.log(resultado); 
